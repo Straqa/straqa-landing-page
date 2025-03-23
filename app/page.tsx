e3b2453b1ccfc329d6b92e74cef02cbaeeb20af0
Ahumareze <ahumarezeifeanyi001@gmail.com>
2025-03-23 12:51:32 +0100

    implemented UI for whyStraqa section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+
+//components
 import Hero from "./components/Hero";
+import WhyStraqa from "./components/WhyStraqa";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-white">
       <Hero />
+      <WhyStraqa />
     </div>
   );
 }
--- components/general/Header.tsx
+++ components/general/Header.tsx
@@ -3,15 +3,18 @@ import Button from "./Button";
 
 export default function Header(){
     return(
-        <div className="flex items-center justify-between px-[20px] max-w-[1200px] mx-auto py-10">
+        <div className="flex items-center justify-between px-[20px] max-w-[1200px] mx-auto py-5 md:py-10">
             <a href={paths.home}>
                 <img
                     src="/straqa-logo.svg"
                     alt=""
-                    className="h-[20px] md:h-[28px]"
+                    className="h-[28px]"
                 />
             </a>
-            <div className="flex items-center justify-end text-[#222D39] text-lg font-medium gap-10">
+            <div className="flex md:hidden items-center justify-end">
+                <Button text="Sign up" />
+            </div>
+            <div className="hidden md:flex items-center justify-end text-[#222D39] text-lg font-medium gap-10">
                 <a href={paths.home}>Payments</a>
                 <a href={paths.home}>Events</a>
                 <a href={paths.home}>eCommerce</a>
