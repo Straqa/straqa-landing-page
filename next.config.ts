4c9a77de14a0a1d6dec7bc3b012e9034b63af3a2
josephchimebuka <chimebujoseph@gmail.com>
2025-04-08 10:44:14 +0100

    added video redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/video',
+        destination: 'https://youtu.be/QPx52UNhkRc', 
+        permanent: false,
+      },
+    ];
+  },
+
 };
 
 export default nextConfig;
